a072d03f8e9aeca43f3c154862eed868f11b5870
Harald Börgmann <h.boergmann@t-online.de>
2020-04-14 19:35:57 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -9,19 +9,17 @@ shuffle(liste)
 for strophe in range(5):
     for n in range(2):
         for i in range(4):
-            print ("spam ", end=' ')
+            print("spam ", end=' ')
         print()
-    #shuffle(liste)
+    # shuffle(liste)
     #print(liste[0] + " SPAM, " +liste[1]+" SPAM")
     #print(liste.pop() + " SPAM, " + liste.pop() + " SPAM")
-    #print("{} SPAM, {} SPAM".format(liste.pop(), liste.pop()))  # {} sind platzhalter
+    # print("{} SPAM, {} SPAM".format(liste.pop(), liste.pop()))  # {} sind platzhalter
     # besser ist
     el1 = liste.pop()
     el2 = liste.pop()
     print("{} SPAM, {} SPAM".format(el1, el2))  # {} sind platzhalter
     print()
 print()
-#ende
-
-
+# ende
 
