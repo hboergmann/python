f20695a61966f29f1717c29e9af630e7478fe0cd
Harald Börgmann <hboerg@hbc-2.fritz.box>
2020-04-19 13:24:35 +0200

    update

---
+++ #_ägyptischer_bruch.py
@@ -0,0 +1,27 @@
+# ägyptischer_bruch (nach Fibonachi)
+# siehe auch Bleicher
+from fractions import Fraction
+def greedy(x,forceOdd = False):
+    L = []
+
+    print(str(x)+ " = ", end="")
+    while x>0:
+        inv = Fraction(1)/x
+        u=int(inv)
+        if u < inv:
+            u += 1
+        if forceOdd and u % 2 == 0:
+            u += 1
+            
+        f = Fraction(1, u)
+        
+        L.append(f)
+        x -= f
+    print(" + ".join(map(str, L)))
+
+greedy(Fraction(20, 21))
+greedy(Fraction(5, 121))
+greedy(Fraction(13, 71))
+
+
+
